cddb3ebe905ad2a1523027f7eeb6388da104602a
Xuan Hung <xuanhung@Xuans-MacBook-Pro.local>
2018-09-20 17:03:58 +0700

    Con tro ham demo

--- main.c
+++ main.c
@@ -4,43 +4,29 @@ int conghaiso(int a, int b) {
     return a + b;
 }
 
+int truhaiso(int a, int b) {
+    return a - b;
+}
+
 int main() {
-    int arrayNumber[3];
-    arrayNumber[0] = 6;
-    arrayNumber[1] = 4;
-    arrayNumber[2] = 7;
-
-    int *myPointer;
-    //myPointer = &arrayNumber[0];
-    myPointer = arrayNumber;
-    printf("arrayNumber[0]: %d\n", arrayNumber[0]);
-    printf("Giá trị contrỏ trỏ tới: %d\n", *myPointer);
-    myPointer++;
-    printf("Giá trị contrỏ trỏ tới: %d\n", *myPointer);
-    myPointer--;
-    printf("Giá trị contrỏ trỏ tới: %d\n", *myPointer);
-    // arrayNumber[2] = 70;
-    myPointer++;
-    myPointer++;
-    *myPointer = 70;
-    printf("arrayNumber[2]: %d\n", arrayNumber[2]);
-
-
-    // Nhập giá trị cho mảng.
-    myPointer = arrayNumber;
-    for (int j = 0; j < 3; ++j) {
-        printf("Vui lòng nhập giá trị cho phần tử thứ %d", j + 1);
-        //scanf("%d", &arrayNumber[j]); // làm việc thông qua index.
-        scanf("%d", myPointer);
-        myPointer++;
+    int (*conTroHam)(int a, int b);
+    printf("Vui lòng lựa chọn phép tính:\n");
+    printf("1. Phép cộng:\n");
+    printf("1. Phép trừ:\n");
+    printf("Nhập lựa chọn của bạn (1|2):\n");
+    int choice;
+    scanf("%d", &choice);
+    switch (choice) {
+        case 1:
+            // do do.
+            conTroHam = &conghaiso;
+            break;
+        case 2:
+            // do something.
+            conTroHam = &truhaiso;
+            break;
     }
-
-    myPointer = arrayNumber;
-    for (int i = 0; i < 3; ++i) {
-        printf("Giá trị của mảng tại phần tử thứ %d là %d\n", i + 1, arrayNumber[i]);// làm việc mảng thông qua inde
-        printf("Giá trị con trỏ là: %d\n", *myPointer); // làm việc thông qua con trỏ.
-        myPointer++;
-    }
+    printf("Kết quả %d\n", conTroHam(49, 59));
     return 0;
 }
 
