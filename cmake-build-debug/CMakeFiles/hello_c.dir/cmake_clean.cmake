548f630cd7a562fdcc75c6e29f8667795f00686f
Xuan Hung <xuanhung@Xuans-MacBook-Pro.local>
2018-09-20 13:55:19 +0700

    New project.

---
+++ cmake-build-debug/CMakeFiles/hello_c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_c.dir/main.c.o"
+  "hello_c.pdb"
+  "hello_c"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hello_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
