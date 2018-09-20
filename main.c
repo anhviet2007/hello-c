2395e05794b0f4d0c98e5a7ba32fc44ff18058d4
Xuan Hung <xuanhung@Xuans-MacBook-Pro.local>
2018-09-20 16:12:08 +0700

    Giai thich lai ve con tro.

--- main.c
+++ main.c
@@ -1,34 +1,26 @@
 #include <stdio.h>
 
-void hoandoi2so(int *conTroCuaC, int *conTroCuaD){
-    // lấy giá trị tại địa chỉ vùng nhớ conTroCuaC trỏ tới.
-    // gán cho bientam kiểu số nguyên.
-    int bientam = *conTroCuaC;
-    *conTroCuaC = *conTroCuaD;
-    *conTroCuaD = bientam;
-    printf("Sau khi hoán đổi, tại hàm hoandoi2so\n");
-    printf("Giá trị tại vùng nhớ của con trỏ C la %d\n", *conTroCuaC);
-    printf("Giá trị tại vùng nhớ của con trỏ D la %d\n", *conTroCuaD);
-}
-
-int conghaiso(int a, int b){
+int conghaiso(int a, int b) {
     return a + b;
 }
 
 int main() {
-    int *conTroCuaC;
-    int *conTroCuaD;
-    int c = 20;
-    int d = 30;
-    conTroCuaC = &c;
-    conTroCuaD = &d;
-    printf("Số thứ nhất là %d\n", c);
-    printf("Số thứ hai là %d\n", d);
-
-    hoandoi2so(conTroCuaC, conTroCuaD);
-    printf("Giá trị của hai số C và D tại hàm main\n");
-    printf("Giá trị số thứ nhất sau khi hoán đổi: %d\n", c);
-    printf("Giá trị số thứ hai sau khi hoán đổi: %d\n", d);
+    int a = 10;
+    int *controA; // khai báo biến con trỏ kiểu int - bắt đầu bằng dấu *
+    controA = &a; // trỏ con trỏ controA đến vị trí bộ nhớ của biến a;
+    printf("Giá trị của a là %d\n", a);// in ra giá trị của a.
+    printf("Giá trị của a là %d\n", &a);// in ra địa chỉ bộ nhớ của a.
+    printf("Giá trị địa chỉ bộ nhớ con trỏ trỏ tới %d\n",
+           *controA);// in ra giá trị mà địa chỉ con trỏ đang trỏ tới chứa.
+    printf("Địa chỉ bộ nhớ mà controA trỏ tới %d\n", controA);
+    *controA = 20; // Thay đổi giá trị mà controA trỏ tới.
+    printf("Giá trị của a là %d\n", a);// in ra giá trị của a.
+    int b = 30; // tạo biến b có giá trị = 30;
+    controA = &b;
+    printf("Giá trị địa chỉ bộ nhớ con trỏ trỏ tới %d\n",
+           *controA);// in ra giá trị mà địa chỉ con trỏ đang trỏ tới chứa.
+    *controA = 50;
+    printf("Giá trị của b là %d", b); // in ra gía trị của b.
     return 0;
 }
 
