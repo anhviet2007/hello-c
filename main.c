67c7bcda060429fd0c4f8481a6becef1a16b8423
Xuan Hung <xuanhung@Xuans-MacBook-Pro.local>
2018-09-22 08:50:32 +0700

    Playing with pointer.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int conghaiso(int a, int b) {
     return a + b;
@@ -9,24 +10,20 @@ int truhaiso(int a, int b) {
 }
 
 int main() {
-    int (*conTroHam)(int a, int b);
-    printf("Vui lòng lựa chọn phép tính:\n");
-    printf("1. Phép cộng:\n");
-    printf("1. Phép trừ:\n");
-    printf("Nhập lựa chọn của bạn (1|2):\n");
-    int choice;
-    scanf("%d", &choice);
-    switch (choice) {
-        case 1:
-            // do do.
-            conTroHam = &conghaiso;
-            break;
-        case 2:
-            // do something.
-            conTroHam = &truhaiso;
-            break;
+    int *ap;
+    *ap = 1;
+    ap++;
+    *ap = 2;
+    ap++;
+    *ap = 3;
+    for (int i = 0; i < 3; ++i) {
+        printf("%d\n", *ap);
+        ap--;
     }
-    printf("Kết quả %d\n", conTroHam(49, 59));
+    ap++;
+    printf("%d\n", *ap);
+    ap++;
+    printf("%d\n", *ap);
     return 0;
 }
 
