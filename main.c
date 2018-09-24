41d6cf0733458873cbc7a7862b1e34fa9d8f47ac
Xuan Hung <xuanhung@Xuans-MacBook-Pro.local>
2018-09-24 17:13:01 +0700

    string handle

--- main.c
+++ main.c
@@ -1,38 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int conghaiso(int a, int b) {
-    return a + b;
-}
-
-int truhaiso(int a, int b) {
-    return a - b;
+void noiHoVaTen(char ho[], char ten[]) {
+    printf("Do dai cua ho la: %d\n", strlen(ho));
+    printf("Do dai cua ten la: %d\n", strlen(ten));
+    int dodaiHo = strlen(ho);
+    int dodaiTen = strlen(ten);
+    int tong = dodaiHo + dodaiTen;
+    printf("Do dai cua ca ho va ten la %d\n", tong);
+    tong++;
+    char hovaten[tong];
+    strcat(hovaten, ho);
+    strcat(hovaten, " ");
+    strcat(hovaten, ten);
+    printf("%s", hovaten);
 }
 
 int main() {
-    char myName[20];
-    char *pointToChar = (char *) malloc(20 * sizeof(char));
-    *pointToChar = 'x';
-    pointToChar++;
-    *pointToChar = 'u';
-    pointToChar++;
-    *pointToChar = 'a';
-    pointToChar++;
-    *pointToChar = 'n';
-    pointToChar++;
-    *pointToChar = ' ';
-    pointToChar++;
-    *pointToChar = 'h';
-    pointToChar++;
-    *pointToChar = 'u';
-    pointToChar++;
-    *pointToChar = 'n';
-    pointToChar++;
-    *pointToChar = 'g';
-    for (int i = 0; i < 20; ++i) {
-        printf("%c", *pointToChar);
-        pointToChar--;
-    }
+    char ho[50], ten[50];
+    printf("Vui long nhap ho cua ban: ");
+    gets(ho);
+    // kiểm tra độ dài họ.
+    printf("Vui long nhap ten cua ban: ");
+    gets(ten);
+    // kiểm tra độ dài tên.
+    printf("Ho va ten cua ban la: ");
+    noiHoVaTen(ho, ten);
     return 0;
 }
-
