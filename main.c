c894647e80f8547bccbd8a7a97e0cbb0d3521e15
LuyenDH <luyendh@fpt.edu.vn>
2018-09-26 21:20:36 +0700

    Demo if else, function declare and use, demo variable

--- main.c
+++ main.c
@@ -1,33 +1,42 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
-void noiHoVaTen(char ho[], char ten[]) {
-    printf("Do dai cua ho la: %d\n", strlen(ho));
-    printf("Do dai cua ten la: %d\n", strlen(ten));
-    int dodaiHo = strlen(ho);
-    int dodaiTen = strlen(ten);
-    int tong = dodaiHo + dodaiTen;
-    printf("Do dai cua ca ho va ten la %d\n", tong);
-    tong++;
-    char hovaten[tong];
-    strcpy(hovaten, ho);
-    strcat(hovaten, " ");
-    strcat(hovaten, ten);
-    printf("%s", hovaten);
+void sayHello() {
+    printf("Hello");
 }
 
+// khai báo và định nghĩa hàm.
+int congHaiSo(int x, int y) {
+    return x + y;
+}
+
+int truHaiSo(int x, int y) {
+    return x - y;
+}
+
+
 int main() {
-    char ho[10], ten[10];
-    printf("Vui long nhap ho cua ban: ");
-    fgets(ho, 10, stdin); // gets
-    puts(ho);
-    if (!strchr(ho, '\n')) {
-        while (fgetc(stdin) != '\n');
+    int a, b, ketqua, luachon;
+    printf("Vui lòng nhập số thứ nhất: ");
+    scanf("%d", &a);
+    printf("Vui lòng nhập số thứ hai: ");
+    scanf("%d", &b);
+
+    printf("Lựa chọn phép tính: \n");
+    printf("1. Phép cộng. \n");
+    printf("2. Phép trừ. \n");
+    printf("Nhập lựa chọn của bạn (1|2): \n");
+    scanf("%d", &luachon);
+    //
+    if (luachon == 1) {
+        printf("Tổng hai số là: ");
+        ketqua = congHaiSo(a, b);
+    } else if (luachon == 2) {
+        printf("Hiệu hai số là: ");
+        ketqua = truHaiSo(a, b);
+    } else {
+        printf("Lựa chọn sai.");
     }
-    printf("Vui long nhap ten cua ban: ");
-    fgets(ten, 10, stdin);
-    puts(ten);
-    noiHoVaTen(ho, ten);
+
+    printf("%d", ketqua);
     return 0;
 }
