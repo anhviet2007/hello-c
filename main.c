0cc34da2fca72d8e72462c1e46fc05c26474d4eb
LuyenDH <luyendh@fpt.edu.vn>
2018-09-27 16:20:43 +0700

    File handle

--- main.c
+++ main.c
@@ -1,42 +1,30 @@
 #include <stdio.h>
-
-void sayHello() {
-    printf("Hello");
-}
-
-// khai báo và định nghĩa hàm.
-int congHaiSo(int x, int y) {
-    return x + y;
-}
-
-int truHaiSo(int x, int y) {
-    return x - y;
-}
-
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-    int a, b, ketqua, luachon;
-    printf("Vui lòng nhập số thứ nhất: ");
-    scanf("%d", &a);
-    printf("Vui lòng nhập số thứ hai: ");
-    scanf("%d", &b);
-
-    printf("Lựa chọn phép tính: \n");
-    printf("1. Phép cộng. \n");
-    printf("2. Phép trừ. \n");
-    printf("Nhập lựa chọn của bạn (1|2): \n");
-    scanf("%d", &luachon);
-    //
-    if (luachon == 1) {
-        printf("Tổng hai số là: ");
-        ketqua = congHaiSo(a, b);
-    } else if (luachon == 2) {
-        printf("Hiệu hai số là: ");
-        ketqua = truHaiSo(a, b);
-    } else {
-        printf("Lựa chọn sai.");
+    // ghi file.
+    FILE *fp = fopen("hello_c.txt", "w+");
+    char choice;
+    char userInput[255];
+    printf("Enter something to save file:\n"); // Yêu cầu nhập.
+    while (1 == 1) {
+        fgets(userInput, 255, stdin);
+        //"exit"
+        if (strcmp(userInput, "exit\n") == 0) {
+            break;
+        }
+        fprintf(fp, userInput);
     }
+    fclose(fp);
 
-    printf("%d", ketqua);
+    // đọc file.
+    printf("--------Read file data------\n");
+    fp = fopen("hello_c.txt", "r");
+    int bufferSize = 255;
+    char buffer[bufferSize];
+    while (fgets(buffer, bufferSize, fp) != NULL) {
+        printf("%s", buffer);
+    }
     return 0;
 }
